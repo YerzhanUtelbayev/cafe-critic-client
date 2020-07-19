af2422ac7105069cf622bce048f5004cea96dbe7
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-07-19 17:11:33 +0600

    Add route to PlaceFullInfo component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,20 +5,28 @@ import Register from './containers/Authorization/Register';
 import Login from './containers/Authorization/Login';
 import AddPlace from './containers/AddPlace/AddPlace';
 import Places from './containers/Places/Places';
+import PlaceFullInfo from './containers/PlaceFullInfo/PlaceFullInfo';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact>
       <Places />
     </Route>
+    <Route path="/places" exact>
+      <Places />
+    </Route>
+    <Route path="/places/new" exact>
+      <AddPlace />
+    </Route>
+    <Route path="/places/:id" exact>
+      <PlaceFullInfo />
+    </Route>
+
     <Route path="/register" exact>
       <Register />
     </Route>
     <Route path="/login" exact>
       <Login />
-    </Route>
-    <Route path="/places/new" exact>
-      <AddPlace />
     </Route>
   </Switch>
 );
