e280380a2b62a83e218a64bea412022fcd7d16bf
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-07-29 17:46:51 +0600

    Switch app name in navbar to home Link

--- src/components/Navigation/NavBar/NavBar.jsx
+++ src/components/Navigation/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import {
-  AppBar, Container, Toolbar, Typography,
+  AppBar, Container, Toolbar, Typography, Link,
 } from '@material-ui/core';
 import AnonymousMenu from '../Menus/HeaderMenus/AnonymousMenu';
 
@@ -8,7 +9,9 @@ const NavBar = () => (
   <AppBar position="static">
     <Container>
       <Toolbar>
-        <Typography variant="h6">Cafe Critic</Typography>
+        <Link color="inherit" underline="none" component={RouterLink} to="/">
+          <Typography variant="h6">Cafe Critic</Typography>
+        </Link>
         <AnonymousMenu />
       </Toolbar>
     </Container>
